c092865a220957fe34f917e87b808c9068e980a2
Oumayma Dhehibi <oumayma.dhehibi@esprit.tn>
2021-01-06 14:48:48 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, {Fragment} from 'react';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
-  return (
-    <div></div>
-  );
+    return (
+        <Fragment>
+            <div className="container pt-5">
+                <div className="columns">
+                    <div className="column is-half is-offset-one-quarter">
+                        <Form/>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
---
+++ src/components/Step.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Step = () => {
+
+}
+
+export default Step;
